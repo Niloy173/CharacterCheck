e49096cbb6655c407b5e5a144f503135fd7c5edc
Your Name <niloysaha173@gmail.com>
2021-12-03 19:30:12 +0600

    Fun project

---
+++ script.js
@@ -0,0 +1,18 @@
+function setBack()
+{
+    const value = Math.floor(Math.random()*16777215).toString(16);
+
+    document.body.style.backgroundColor = "#"+ value;
+}
+
+setInterval(()=>{
+    setBack();
+},2000);
+
+
+document.getElementById("click").addEventListener("click",function(){
+
+    let val = document.getElementById('userValue').value;
+    // alert(val);
+    document.getElementById('image').setAttribute("src",`https://joeschmoe.io/api/v1/${val}`);
+})
